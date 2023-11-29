e36ed5def203cdf5870ec819bd74974fa68af6dd
SarperKececi <loops4terso@gmail.com>
2023-11-30 01:17:40 +0300

    Initial Commit

---
+++ PokedexII/PokedexIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexIIApp.swift
+//  PokedexII
+//
+//  Created by Sarper Kececi on 30.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct PokedexIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
